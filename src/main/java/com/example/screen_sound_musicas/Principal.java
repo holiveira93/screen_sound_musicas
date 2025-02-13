ca0df070dc29d842d32c850c155bdc49e2a50c52
holiveira93 <holiveira_93@hotmail.com>
2025-02-13 12:03:15 -0300

    created principal menu

--- src/main/java/com/example/screen_sound_musicas/Principal.java
+++ src/main/java/com/example/screen_sound_musicas/Principal.java
@@ -1,4 +1,48 @@
 package com.example.screen_sound_musicas;
 
+import java.util.Scanner;
+
 public class Principal {
+    Scanner scan = new Scanner(System.in);
+
+    public void menu (){
+        boolean repeat = true;
+        do{
+            System.out.println("""
+                1 - Register artists
+                2 - Register musics
+                3 - List musics
+                4 - Find musics by artist
+                5 - Find data about a artist
+                0 - Exit
+                """);
+            System.out.println(">> ");
+            int menu = scan.nextInt();
+
+            switch (menu){
+                case 0:
+                    repeat = false;
+                    break;
+                case 1:
+                    registerArtists();
+                    break;
+                case 2:
+                    registerMusics();
+                    break;
+                case 3:
+                    listMusics();
+                    break;
+                case 4:
+                    findMusicsByArtist();
+                    break;
+                case 5:
+                    findArtist();
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }while(repeat);
+
+    }
 }
